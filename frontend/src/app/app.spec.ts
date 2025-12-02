891b9725877c2960ce3f8ac96eea73ba90698841
Xkmarasa <xavicamarasaa@gmail.com>
2025-12-02 18:42:23 +0100

    testing

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -14,7 +14,7 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render router outlet', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
